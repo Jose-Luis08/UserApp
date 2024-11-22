a73a4319ab6eaacf0cec1dd508da0d21a1911cbd
José Luis Reyes Chávez <joseluis.reyeschavez@gmail.com>
2024-11-22 17:11:47 -0600

    Home Screen y Create screen archivos
    Se define unicamente la estructura

---
+++ lib/Home_Screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'create_profile_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Crear perfil'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => CreateProfileScreen()), //Aquí voy a crear la otra screen
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
